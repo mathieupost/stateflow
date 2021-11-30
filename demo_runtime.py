1545a2139df889cea08cb13c5f6e87ccfcccc767
Mathieu Post <mathieupost@gmail.com>
2021-11-30 11:10:17 +0100

    demo: create topics if not exist

--- demo_runtime.py
+++ demo_runtime.py
@@ -1,6 +1,22 @@
+from kafka.admin import KafkaAdminClient, NewTopic
+from kafka.errors import TopicAlreadyExistsError
+
 from demo_common import stateflow
-from stateflow.runtime.beam_runtime import BeamRuntime, Runtime
+from stateflow.runtime.beam_runtime import BeamRuntime
 from stateflow.serialization.pickle_serializer import PickleSerializer
+
+admin_client = KafkaAdminClient(
+    bootstrap_servers="localhost:9092",
+    client_id='test'
+)
+topic_list = [
+    NewTopic(name="client_request", num_partitions=1, replication_factor=1),
+    NewTopic(name="internal", num_partitions=1, replication_factor=1),
+]
+try:
+    admin_client.create_topics(new_topics=topic_list, validate_only=False)
+except TopicAlreadyExistsError:
+    pass
 
 # Initialize stateflow
 flow = stateflow.init()
