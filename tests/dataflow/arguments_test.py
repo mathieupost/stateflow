c4656f0f2d28f797daae8a56a23a4d9eca2abb21
Mathieu Post <mathieupost@gmail.com>
2022-02-04 17:19:06 +0100

    feat(arguments_test): assert args

--- tests/dataflow/arguments_test.py
+++ tests/dataflow/arguments_test.py
@@ -7,42 +7,42 @@ def test_match_simple_args():
     desc = {"x": int}
     args = Arguments.from_args_and_kwargs(desc, 1)
 
-    assert args.get() == {"x": 1}
+    assert args and args.get() == {"x": 1}
 
 
 def test_match_more_args():
     desc = {"x": int, "y": str, "z": list}
     args = Arguments.from_args_and_kwargs(desc, 1, "hi", ["no"])
 
-    assert args.get() == {"x": 1, "y": "hi", "z": ["no"]}
+    assert args and args.get() == {"x": 1, "y": "hi", "z": ["no"]}
 
 
 def test_match_simple_kwargs():
     desc = {"x": int}
     args = Arguments.from_args_and_kwargs(desc, x=1)
 
-    assert args.get() == {"x": 1}
+    assert args and args.get() == {"x": 1}
 
 
 def test_match_more_kwargs():
     desc = {"x": int, "y": str, "z": list}
     args = Arguments.from_args_and_kwargs(desc, x=1, y="hi", z=["no"])
 
-    assert args.get() == {"x": 1, "y": "hi", "z": ["no"]}
+    assert args and args.get() == {"x": 1, "y": "hi", "z": ["no"]}
 
 
 def test_match_mix_args_kwargs():
     desc = {"x": int, "y": str, "z": list}
     args = Arguments.from_args_and_kwargs(desc, 1, y="hi", z=["no"])
 
-    assert args.get() == {"x": 1, "y": "hi", "z": ["no"]}
+    assert args and args.get() == {"x": 1, "y": "hi", "z": ["no"]}
 
 
 def test_match_mix_args_kwargs_more():
     desc = {"x": int, "y": str, "z": list}
     args = Arguments.from_args_and_kwargs(desc, 1, z=["no"], y="hi")
 
-    assert args.get() == {"x": 1, "y": "hi", "z": ["no"]}
+    assert args and args.get() == {"x": 1, "y": "hi", "z": ["no"]}
 
 
 def test_match_wrong_desc():
