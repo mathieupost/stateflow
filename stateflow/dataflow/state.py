0c989a472d08acf88dca69b38b466f28fe063cd4
Mathieu Post <mathieupost@gmail.com>
2022-03-02 15:17:41 +0100

    feat(state): add Store and Version classes
    To keep track of different State versions.

--- stateflow/dataflow/state.py
+++ stateflow/dataflow/state.py
@@ -1,4 +1,5 @@
-from typing import Dict, Any, List
+from typing import Dict, Any, Optional, Tuple
+import jsonpickle
 
 from stateflow.dataflow.address import FunctionAddress
 
@@ -28,6 +29,11 @@ class State:
     def get_version_id(self):
         return self._version_id
 
+    def copy_with_version_id(self, version_id) -> "State":
+        """Copies the state, but overwrites the version_id attribute.
+        """
+        return State(self._data, version_id)
+
 
 class WriteSet(dict):
     def __init__(self, *args, **kwargs):
@@ -48,6 +54,121 @@ class WriteSet(dict):
 
         key = address.key
         self[namespace][operator][key] = version
+
+
+class Version:
+    def __init__(self, id: int, parent_id: int, state: State) -> None:
+        self.id = id
+        self.parent_id = parent_id
+        self.state = state
+        self.write_set = None
+
+    def create_child(self, new_id: int) -> "Version":
+        """Creates a new version based on this version.
+
+        Sets the version id to the given id and the parent id to the id of this
+        version and copies the state.
+        """
+        parent_id = self.id
+        state = self.state.copy_with_version_id(new_id)
+        return Version(new_id, parent_id, state)
+
+    def set_state(self, state: State):
+        """Sets the state of this version to the given state.
+        
+        And updates the version id of the state.
+        """
+        state._version_id = self.id
+        self.state = state
+
+    def set_write_set(self, write_set: WriteSet):
+        self.write_set = write_set
+    
+    def update(self, state: State, write_set: WriteSet):
+        self.set_state(state)
+        self.set_write_set(write_set)
+
+
+class Store:
+    def __init__(self, data=None, initial_state=None) -> None:
+        """Initialized the store object from the given dict.
+        
+        If an initial state is provided, a new Store will be created with the
+        given initial state as version 0."""
+        if initial_state:
+            self.encoded_versions: Dict[int, bytes] = dict()
+            self.last_committed_version_id: int = 0
+            self.event_version_map: Dict[str, int] = dict()
+            initial_version = Version(0, -1, State(initial_state))
+            self.set_version(0, initial_version)
+            return
+
+        self.encoded_versions: Dict[int, bytes] = data["encoded_versions"]
+        self.last_committed_version_id: int = data["last_committed_version_id"]
+        self.event_version_map: Dict[str, int] = data["event_version_map"]
+
+    def create_new_version(self) -> Tuple[int, Version]:
+        """Create a new version based on the last committed version.
+
+        Increments the highest available version id to use as id for the new
+        version.
+
+        :return: the version id and the new version.
+        """
+        # Add 1 to the currently highest version id
+        new_id = max(self.encoded_versions.keys()) + 1
+
+        # Copy state from last committed state
+        last_version = self.get_version(self.last_committed_version_id)
+        version = last_version.create_child(new_id)
+
+        return new_id, version
+
+    def set_version(self, id: int, version: Version):
+        """Encodes and sets the given version.
+
+        :param id: the id of the version to set.
+        :param version: the version to set.
+        """
+        encoded_version: bytes = jsonpickle.encode(version)
+        self.encoded_versions[id] = encoded_version
+
+    def get_version(self, id: int) -> Version:
+        """Gets the encoded version with the given id and decodes it into a
+        Version object.
+
+        :param id: the id of the version to retrieve.
+        :return: a Version.
+        """
+        encoded_version = self.encoded_versions[id]
+        return jsonpickle.decode(encoded_version)
+
+    def get_version_for_event(self, event_id: str) -> Version:
+        """Returns the version for the given event id.
+
+        If no version exists yet for the given event id, a new version will be
+        created.
+
+        :param event_id: the id of the event to get the corresponding version for.
+        :return: the version id and the version of the given event.
+        """
+        if event_id in self.event_version_map:
+            id = self.event_version_map[event_id]
+            version = self.get_version(id)
+            return version
+        else:
+            id, version = self.create_new_version()
+            self.event_version_map[event_id] = id
+            return version
+
+    def commit_version(self, version_id):
+        # TODO: check if last committed version is not changed (not different from base id)
+        self.last_committed_version_id = version_id
+
+    def update_version_for_event(self, event_id: str, version: Version):
+        id = self.event_version_map[event_id]
+        self.set_version(id, version)
+        self.commit_version(id) # TODO: this should be done on receiving a Request.COMMIT
 
 
 class StateDescriptor:
