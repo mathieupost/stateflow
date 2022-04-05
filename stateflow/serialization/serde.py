c53b5dfb8be01e22dc3c55189e8246192a4dd17d
Mathieu Post <mathieupost@gmail.com>
2022-04-05 16:03:37 +0200

    feat(SerDe): add (de)serialize_store methods

--- stateflow/serialization/serde.py
+++ stateflow/serialization/serde.py
@@ -1,9 +1,19 @@
-from stateflow.dataflow.event import Event
 import abc
 from typing import Dict
 
+from stateflow.dataflow.event import Event
+from stateflow.dataflow.state import Store
+
 
 class SerDe(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def serialize_store(self, store: Store) -> bytes:
+        pass
+
+    @abc.abstractmethod
+    def deserialize_store(self, store: bytes) -> Store:
+        pass
+
     @abc.abstractmethod
     def serialize_event(self, event: Event) -> bytes:
         pass
