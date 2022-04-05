085c8a5f4d87317889f4dc7bc212942d08fa758a
Mathieu Post <mathieupost@gmail.com>
2022-04-05 16:04:38 +0200

    feat(JsonSerializer): implement (de)serialize_store

--- stateflow/serialization/json_serde.py
+++ stateflow/serialization/json_serde.py
@@ -1,11 +1,30 @@
-from stateflow.serialization.serde import SerDe, Event, Dict
+import ujson
 from stateflow.dataflow.args import Arguments
 from stateflow.dataflow.event import EventType, FunctionAddress
 from stateflow.dataflow.event_flow import EventFlowGraph
-import ujson
+from stateflow.dataflow.state import Store
+from stateflow.serialization.serde import Event, SerDe
 
 
 class JsonSerializer(SerDe):
+    def serialize_store(self, store: Store) -> bytes:
+        encoded_versions = store.encoded_versions
+        last_committed_version_id = store.last_committed_version_id
+        event_version_map = store.event_version_map
+
+        store_dict = {
+            "encoded_versions": encoded_versions,
+            "last_committed_version_id": last_committed_version_id,
+            "event_version_map": event_version_map,
+        }
+        return self.serialize_dict(store_dict)
+
+    def deserialize_store(self, store: bytes) -> Store:
+        store_dict = self.deserialize_dict(store)
+        if type(store_dict) is Store:
+            return store_dict
+        return Store(store_dict)
+
     def serialize_event(self, event: Event) -> bytes:
         event_id: str = event.event_id
         event_type: str = event.event_type.value
@@ -16,14 +35,14 @@ class JsonSerializer(SerDe):
             if hasattr(payload[item], "to_dict"):
                 payload[item] = payload[item].to_dict()
 
-        event = {
+        event_dict = {
             "event_id": event_id,
             "event_type": event_type,
             "fun_address": fun_address,
             "payload": payload,
         }
 
-        return bytes(self.serialize_dict(event), "utf-8")
+        return self.serialize_dict(event_dict)
 
     def deserialize_event(self, event: bytes) -> Event:
         json = self.deserialize_dict(event)
@@ -41,8 +60,8 @@ class JsonSerializer(SerDe):
 
         return Event(event_id, fun_address, event_type, payload)
 
-    def serialize_dict(self, dictionary: Dict) -> bytes:
-        return ujson.encode(dictionary)
+    def serialize_dict(self, dictionary: dict) -> bytes:
+        return ujson.encode(dictionary, ensure_ascii=False).encode("utf-8")
 
-    def deserialize_dict(self, dictionary: bytes) -> Dict:
+    def deserialize_dict(self, dictionary: bytes) -> dict:
         return ujson.decode(dictionary)
